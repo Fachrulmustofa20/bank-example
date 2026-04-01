b1881ff4503980fa2a0c03dfb6733af827b08402
Fachrulmustofa20 <fachrulmustofa100@gmail.com>
2026-04-01 15:08:28 +0700

    refactor constant

---
+++ constants/errors.go
@@ -0,0 +1,11 @@
+package constants
+
+import "errors"
+
+var (
+	ErrResponse         = errors.New("sign in to proceed")
+	ErrExpired          = errors.New("token expired. please login again")
+	ErrBalanceNotEnough = errors.New("your bank balance is not enough")
+	ErrLogin            = errors.New("invalid email/password")
+	ErrCodeBank         = errors.New("invalid code in bank")
+)
